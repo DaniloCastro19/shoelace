d5304c6c27b26c998c9a22647e6a16c06c18c1ee
DaniloCastro19 <danyandresc@gmail.com>
2026-07-15 10:50:30 -0500

    feat(pagination): Add acessibility and render tests

--- src/components/pagination/pagination.test.ts
+++ src/components/pagination/pagination.test.ts
@@ -3,11 +3,38 @@ import { expect, fixture, html } from '@open-wc/testing';
 import type SlPagination from './pagination.component.js';
 
 describe('<sl-pagination>', () => {
+  interface pageProps {
+    page: number;
+  }
   it('should render a component', async () => {
     const el = await fixture<SlPagination>(html` <sl-pagination></sl-pagination> `);
 
+    await expect(el).is.accessible();
+    expect(el.shadowRoot!.querySelector('button')).to.exist;
     expect(el.page).to.equal(1);
     expect(el.total).to.equal(1);
     expect(el.disabled).to.equal(false);
+  });
+
+  it('render the right number of elements for a given total', async () => {
+    let page = 1;
+    const total = 5;
+    const setCurrentPage = (newPage: number) => {
+      page = newPage;
+    };
+    const el = await fixture<SlPagination>(html`
+      <sl-pagination
+        page=${page}
+        total=${total}
+        onSlChange=${(event: CustomEvent<pageProps>) => setCurrentPage(event.detail.page)}
+      >
+      </sl-pagination>
+    `);
+
+    
+    const totalBtnsRendered = el.shadowRoot!.querySelectorAll('button').length;
+    console.log(el.shadowRoot!.querySelectorAll('button'));
+
+    expect(totalBtnsRendered).to.equal(total + 2); // Total pages buttom + Prev & Next buttoms
   });
 });
